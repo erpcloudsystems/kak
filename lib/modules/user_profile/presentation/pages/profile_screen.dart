1e0d1c285791a06f7968be62971cc3830a0b409b
Mohamed_Mohamedy <mohamedmohamedy146@gmail.com>
2023-10-26 15:47:39 +0300

    Add user name section to user profile

--- lib/modules/user_profile/presentation/pages/profile_screen.dart
+++ lib/modules/user_profile/presentation/pages/profile_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import '../widgets/photo_widget.dart';
+import '../widgets/user_name_section.dart';
 import '../../../../core/resources/strings_manager.dart';
 import '../../../../core/utils/custom_scrolling_animated_template.dart';
 
@@ -13,6 +14,7 @@ class ProfilePage extends StatelessWidget {
       appBar: AppBar(title: const Text(StringsManager.profile)),
       body: const CustomScrollingAnimatedTemplate(children: [
         UserPhotoWidget(),
+        UserNameSection(),
       ]),
     );
   }
