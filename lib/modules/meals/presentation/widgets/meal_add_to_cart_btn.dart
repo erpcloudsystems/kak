ba8de7e7c665b20a0c59290fcd0ce8684ce51364
Mohamed_Mohamedy <mohamedmohamedy146@gmail.com>
2023-12-06 19:59:46 +0200

    fix add offer to cart and navigating bugs

--- lib/modules/meals/presentation/widgets/meal_add_to_cart_btn.dart
+++ lib/modules/meals/presentation/widgets/meal_add_to_cart_btn.dart
@@ -1,15 +1,10 @@
-
-import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter/material.dart';
 import 'package:uuid/uuid.dart';
 
-import '../../../../core/resources/colors_manager.dart';
-import '../../../../core/resources/routes.dart';
-import '../../../../core/utils/enums.dart';
-import '../../../../core/utils/snack_bar_util.dart';
-import '../../../Cart/presentation/bloc/cart_bloc.dart';
 import '../../domain/entities/meal_entity.dart';
 import 'contents_widgets/custom_bottom_button.dart';
+import '../../../Cart/presentation/bloc/cart_bloc.dart';
 
 class MealAddToCartBut extends StatelessWidget {
   const MealAddToCartBut({
@@ -23,40 +18,21 @@ class MealAddToCartBut extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return BlocListener<CartBloc, CartState>(
-      listenWhen: (previous, current) =>
-          previous.addCartItemState != current.addCartItemState,
-      listener: (context, state) {
-        if (state.addCartItemState == RequestState.success) {
-          SnackBarUtil().getSnackBar(
-            context: context,
-            message: state.addCartItemMessage,
-            color: ColorsManager.gGreen,
-          );
-          Navigator.of(context).pushNamed(Routes.cartScreenKey);
-        }
-
-        if (state.addCartItemState == RequestState.error) {
-          SnackBarUtil().getSnackBar(
-            context: context,
-            message: state.addCartItemMessage,
-            color: ColorsManager.gRed,
-          );
-        }
-      },
-      child: MealCustomBottomButton(
-          total: context.watch<CartBloc>().state.totalPrice.toString(),
-          onPressed: () {
-            BlocProvider.of<CartBloc>(context).add(AddCartItemEvent(
-                meal: MealEntity(
-              description: meal.description,
-              imageUrl: meal.imageUrl,
-              price: meal.priceAfterDiscount!,
-              name: meal.name,
-              id: const Uuid().v4(),
-              quantity: quantity.value,
-            )));
-          }),
-    );
+    return MealCustomBottomButton(
+        total: context.watch<CartBloc>().state.totalPrice.toString(),
+        onPressed: () {
+          // To close the bottom sheet first before navigating.
+          Navigator.of(context).pop();
+          BlocProvider.of<CartBloc>(context).add(AddCartItemEvent(
+              meal: MealEntity(
+            description: meal.description,
+            imageUrl: meal.imageUrl,
+            price: meal.priceAfterDiscount!,
+            name: meal.name,
+            id: const Uuid().v4(),
+            quantity: quantity.value,
+            components: const [],
+          )));
+        });
   }
 }
