23a14e8a0e9a71dda8bb7b0d36cfb197b994e4ff
Mohamed_Mohamedy <mohamedmohamedy146@gmail.com>
2023-12-07 15:57:40 +0200

    Add floating button in items groups screen to navigate to the cart screen

--- lib/modules/meals/presentation/pages/meal_group_items.dart
+++ lib/modules/meals/presentation/pages/meal_group_items.dart
@@ -4,7 +4,10 @@ import 'package:flutter/material.dart';
 import '../bloc/meals_bloc.dart';
 import '../../../../core/utils/enums.dart';
 import '../../../../core/utils/no_data.dart';
+import '../../../../core/resources/routes.dart';
 import '../../../../core/resources/assetss_path.dart';
+import '../../../../core/resources/colors_manager.dart';
+import '../../../../core/resources/values_manager.dart';
 import '../../../../core/resources/strings_manager.dart';
 import '../../../../core/utils/custom_animated_grid.dart';
 
@@ -25,7 +28,8 @@ class MealGroupItemsScreen extends StatelessWidget {
             previous.getMealGroupItemsData != current.getMealGroupItemsData,
         builder: (context, state) {
           if (state.getMealGroupItemsState == RequestState.error) {
-            return const Center(child: NoDataWidget(
+            return const Center(
+                child: NoDataWidget(
               assetPath: ImagesPath.errorPath,
               text: StringsManager.errorMessage,
             ));
@@ -36,6 +40,17 @@ class MealGroupItemsScreen extends StatelessWidget {
 
           return Container();
         },
+      ),
+      floatingActionButton: Padding(
+        padding: const EdgeInsets.only(bottom: DoubleManager.d_30),
+        child: FloatingActionButton(
+          backgroundColor: ColorsManager.swatchRed,
+          foregroundColor: ColorsManager.gWhite,
+          shape: const CircleBorder(),
+          onPressed: () =>
+              Navigator.of(context).pushNamed(Routes.cartScreenKey),
+          child: const Icon(Icons.shopping_cart_outlined),
+        ),
       ),
     );
   }
