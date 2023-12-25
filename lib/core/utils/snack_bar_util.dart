450e02c521135c8dd1336ec6b02e32100998b096
Mohamed_Mohamedy <mohamedmohamedy146@gmail.com>
2023-12-25 13:17:46 +0200

    Add logic to close the old snackbar and display the new one if any

--- lib/core/utils/snack_bar_util.dart
+++ lib/core/utils/snack_bar_util.dart
@@ -1,18 +1,24 @@
 import 'package:flutter/material.dart';
 
+import '../resources/colors_manager.dart';
+
 class SnackBarUtil {
   void getSnackBar({
     required BuildContext context,
     required String message,
     required Color color,
   }) {
+    
+    // Close the old SnackBar
+    ScaffoldMessenger.of(context).hideCurrentSnackBar();
+
     ScaffoldMessenger.of(context).showSnackBar(
       SnackBar(
         content: Text(message,
             style: Theme.of(context)
                 .textTheme
                 .bodyMedium!
-                .copyWith(color: Colors.white)),
+                .copyWith(color: ColorsManager.gWhite)),
         backgroundColor: color,
       ),
     );
