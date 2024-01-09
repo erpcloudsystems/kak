9341bf47dbe7aa732d762e3ecb04f8bf8abb4876
Mohamed_Mohamedy <mohamedmohamedy146@gmail.com>
2024-01-09 13:45:35 +0200

    link taxes with UI

--- lib/modules/Payment/presentation/widgets/checkout/price_summary.dart
+++ lib/modules/Payment/presentation/widgets/checkout/price_summary.dart
@@ -1,16 +1,65 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:kak/modules/Cart/presentation/bloc/cart_bloc.dart';
 
+import '../../bloc/payment_bloc.dart';
+import '../../../../../core/utils/enums.dart';
+import '../../../../../core/utils/error_dialog.dart';
 import '../../../../../core/resources/fonts_manager.dart';
+import '../../../../Cart/presentation/bloc/cart_bloc.dart';
 import '../../../../../core/resources/values_manager.dart';
 import '../../../../../core/resources/strings_manager.dart';
 
-class PriceSummary extends StatelessWidget {
+class PriceSummary extends StatefulWidget {
   const PriceSummary({super.key});
 
   @override
+  State<PriceSummary> createState() => _PriceSummaryState();
+}
+
+class _PriceSummaryState extends State<PriceSummary> {
+  @override
   Widget build(BuildContext context) {
+    return BlocBuilder<PaymentBloc, PaymentState>(
+      buildWhen: (previous, current) =>
+          previous.getTaxesState != current.getTaxesState,
+      builder: (context, state) {
+        switch (state.getTaxesState) {
+          case RequestState.loading:
+            return const Center(child: CircularProgressIndicator());
+          case RequestState.error:
+            ErrorDialogUtils.displayErrorDialog(
+                context: context, errorMessage: state.getTaxesMessage);
+            return const SizedBox();
+          case RequestState.success:
+            return PaymentSummarySuccessWidget(
+              deliveryFee: state.getTaxesData.deliverFee,
+              taxes: state.getTaxesData.taxes,
+            );
+          default:
+            return const SizedBox();
+        }
+      },
+    );
+  }
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+    context.read<PaymentBloc>().add(GetTaxesEvent());
+  }
+}
+
+class PaymentSummarySuccessWidget extends StatelessWidget {
+  const PaymentSummarySuccessWidget({
+    super.key,
+    required this.deliveryFee,
+    required this.taxes,
+  });
+
+  final double deliveryFee, taxes;
+  @override
+  Widget build(BuildContext context) {
+    final double subtotal = context.read<CartBloc>().state.totalPrice;
     return Padding(
       padding: const EdgeInsets.symmetric(
           horizontal: DoubleManager.d_8, vertical: DoubleManager.d_20),
@@ -26,19 +75,19 @@ class PriceSummary extends StatelessWidget {
           ),
           PaymentSummaryComponent(
             title: StringsManager.subtotal(context),
-            amount: context.read<CartBloc>().state.totalPrice,
+            amount: subtotal,
           ),
-           PaymentSummaryComponent(
+          PaymentSummaryComponent(
             title: StringsManager.deliveryFee(context),
-            amount: 30,
+            amount: deliveryFee,
           ),
-           PaymentSummaryComponent(
+          PaymentSummaryComponent(
             title: StringsManager.tax(context),
-            amount: 30,
+            amount: taxes,
           ),
-           PaymentSummaryComponent(
+          PaymentSummaryComponent(
             title: StringsManager.totalAmount(context),
-            amount: 201,
+            amount: subtotal + deliveryFee + taxes,
           ),
         ],
       ),
