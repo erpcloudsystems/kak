2a0cd130688a8b82b454a23fbb02796678492672
Mohamed_Mohamedy <mohamedmohamedy146@gmail.com>
2024-01-21 17:56:30 +0200

    Link signing with Google with social login flow

--- lib/modules/Authentication/presentation/widgets/sign_screen_components/social_sign_components/google_sign_widget.dart
+++ lib/modules/Authentication/presentation/widgets/sign_screen_components/social_sign_components/google_sign_widget.dart
@@ -3,9 +3,10 @@ import 'package:flutter/material.dart';
 import 'package:sizer/sizer.dart';
 
 import '../../../../../../core/utils/enums.dart';
-import '../../../../../../core/resources/routes.dart';
 import '../../../bloc/social_sign/social_sign_bloc.dart';
 import '../../../../../../core/utils/snack_bar_util.dart';
+import '../../../../../../core/global/global_varibles.dart';
+import '../../../bloc/regular_sign/authentication_bloc.dart';
 import '../../../../../../core/resources/values_manager.dart';
 import '../../../../../../core/utils/loading_indicator_util.dart';
 
@@ -21,20 +22,19 @@ class GoogleSignWidget extends StatelessWidget {
       listener: (context, state) {
         if (state.googleSignState == RequestState.error) {
           SnackBarUtil().getSnackBar(
-              context: context,
-              message: state.googleSignMessage,
-              color: Colors.red);
+            context: context,
+            message: state.googleSignMessage,
+            color: Colors.red,
+          );
         }
         if (state.googleSignState == RequestState.success) {
-             SnackBarUtil().getSnackBar(
-              context: context,
-              message: 'Success sign with Google!',
-              color: Colors.green);
-          // BlocProvider.of<CachingUserDataBloc>(context).add(
-          //   CacheUserDataEvent(userEmail: state.googleUserData.email),
-          // );
-          Navigator.of(context)
-              .pushNamedAndRemoveUntil(Routes.navigationBarScreenKey, (route) => false);
+          /* Here we save the user password in the global variables to use it with auto-login
+          and make the choice be true to be remembered, and then call the event.*/
+          final gv = GlobalVariables();
+          final user = state.googleUserData;
+          gv.setGlobalUserPassword = state.googleUserData.password;
+          gv.setUserDecision = true;
+          context.read<AuthenticationBloc>().add(SignBySocialEvent(user: user));
         }
       },
       buildWhen: (previous, current) =>
